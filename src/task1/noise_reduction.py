9767986645eaff5efa37005af4c51b1f281163ae
FlashTek <rzrolandzimmermann@gmail.com>
2018-02-18 13:07:04 +0100

    added more comment to code of task 1

--- src/task1/noise_reduction.py
+++ src/task1/noise_reduction.py
@@ -1,3 +1,7 @@
+"""
+    Tests how well noisy images can be cleared by using morphological operations
+"""
+
 import numpy as np
 import matplotlib.pyplot as plt
 import morphological_operations as mo
@@ -6,7 +10,7 @@ from PIL import Image
 import matplotlib
 from cycler import cycler
 
-# plotting parameters
+#plotting parameters
 
 matplotlib.rc('text', usetex = True)
 font = {'family':'serif','size':11}
@@ -14,21 +18,28 @@ plt.rc('font',**font)
 figsize = (6.29 , 3.54) # = 16*9 cm
 dpi = 150
 
-# gaussian noise example
-
+#gaussian noise example
+#creates a black image with gaussian noise
 to_denoise = np.zeros((100, 100))  + np.random.randn(100,100)
 
+#create one white horizontal bar
 to_denoise[50:] = to_denoise[50:] + 10
 
+#normalize the image
 to_denoise = to_denoise - np.min(to_denoise)
+
+#define the structuing element
 SE = se.square(5)
 
+#apply a closing operation to remove the noise
 denoised = mo.closing(to_denoise, SE)
 
+#test different SE again with an opening operation
 SE1 = se.square(9)
 denoised1 = mo.opening(to_denoise, SE)
 
 
+#show the resulting images together with the original image
 fig, ax = plt.subplots(1,3, figsize = (figsize[0] , 2.2), dpi  = dpi)
 
 ax[0].imshow(to_denoise, cmap = 'Greys_r')
@@ -43,19 +54,27 @@ fig.subplots_adjust(left = 0.02, right = 0.98, hspace = 0.01)
 for a in ax:
      a.axis('off')
 
+#uncomment this line to save the output
 #fig.savefig("closing_gauss.pdf")
 
+#create second example
+#create empty image with horizontal bar
 to_denoise = np.zeros((100, 100))
 to_denoise[50:] = 255
 
+#prepare salt and pepper noise (binary noise)
 mask = np.random.random((100, 100))
 mask = [mask > 0.8]
 
 mask_ = np.random.random((100, 100))
 mask_ = [mask_ > 0.8]
 
+#add salt and pepper noise
 to_denoise[mask_] = 255 - np.random.rand(to_denoise[mask_].shape[0]) * 50
 to_denoise[mask] = np.random.rand(to_denoise[mask].shape[0]) * 50
+
+#calculate the results of closing/opening operations now and
+#plot the original image as well as the results of the operations
 
 fig1, ax1 = plt.subplots(1,3, figsize = (figsize[0], 2.2), dpi = dpi)
 fig1.subplots_adjust(left = 0.02, right = 0.98, hspace = 0.01)
@@ -70,6 +89,7 @@ ax1[2].imshow(mo.opening(to_denoise, SE), cmap = 'Greys_r')
 for a in ax1:
     a.axis('off')
 
+#save the resulting image
 fig1.savefig("sp_noise_example.pdf")
 
 plt.show()
--- src/task1/test_morphology.py
+++ src/task1/test_morphology.py
@@ -1,3 +1,8 @@
+"""
+    Tests the effects of the morphological operation (erison, dilation, opening, closing, gradient)
+    on a sample image and plots the results
+"""
+
 import numpy as np
 import matplotlib.pyplot as plt
 import morphological_operations as mo
@@ -6,8 +11,7 @@ from PIL import Image
 import matplotlib
 from cycler import cycler
 
-# plotting parameters
-
+#plotting parameters
 matplotlib.rc('text', usetex = True)
 font = {'family':'serif','size':11}
 plt.rc('font',**font)
@@ -15,14 +19,15 @@ plt.rc('axes', prop_cycle=(cycler('color', ['b', 'r', 'k', 'c', 'm', 'y']) ))
 figsize = (6.29 , 3.54) # = 16*9 cm
 dpi = 150
 
-
+#load input image (MRI scan of a human brain)
 im = np.asarray(Image.open("brain.jpg"))[:,50:-50,0]
 
+#create structuring elements
 square = se.square(5)
 cross = se.cross(5)
 circle = se.circle(5)
 
-
+#prepare plots (set axis settings, etc.)
 fig_mg, ax_mg = plt.subplots(1,4, figsize = (figsize[0], 2.5), dpi = dpi)
 fig_cl, ax_cl = plt.subplots(1,4, figsize = (figsize[0], 2.5), dpi = dpi)
 i = 1
@@ -33,8 +38,8 @@ ax_cl[0].axis('off')
 ax_mg[0].set_title('original')
 ax_cl[0].set_title('original')
 
+#apply the gradient and opening operation for different structuring elements and plot the results
 for se, name  in zip([square, cross, circle], ["square", "cross", "circle"]):
-
     mg = mo.morphological_gradient(im ,se)
     cl = mo.opening(im, se)
     ax_mg[i].imshow(mg, cmap = 'Greys_r')
@@ -48,19 +53,24 @@ for se, name  in zip([square, cross, circle], ["square", "cross", "circle"]):
     i+=1
 
 fig_mg.subplots_adjust(wspace = 0.01, left = 0.01, right= 0.99)
+#save the image
 fig_mg.savefig("morphological_gradient.pdf")
 
 fig_cl.subplots_adjust(wspace = 0.01, left = 0.01, right= 0.99)
+#save the image
 fig_cl.savefig("morphological_closing.pdf")
 
 fig_ed, ax_ed = plt.subplots(3,1, figsize = (figsize[0]/3 , figsize[1] * 1.7))
 
+#plot erosion and dilation
 ax_ed[0].imshow(im, cmap = "Greys_r",  vmin = 0, vmax = 255)
 ax_ed[1].imshow(mo.erosion(im, square), cmap = "Greys_r",  vmin = 0, vmax = 255)
 ax_ed[2].imshow(mo.dilation(im, square), cmap = "Greys_r", vmin = 0, vmax = 255)
 for a in ax_ed:
     a.axis("off")
 fig_ed.subplots_adjust(hspace = 0.02, top = 0.99, bottom = 0.01, right = 0.98, left = 0.02)
+
+#save the resulting image
 fig_ed.savefig("er_dil.pdf")
 
 plt.show()
